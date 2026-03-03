bdb1d25826e22443ee44727d6b382fe50683f38f
kangrae <kangrae0501@gmail.com>
2026-03-03 17:40:55 +0900

    test: 카드 값으로 부터 점수를 계산하는 메서드 테스트 추가

--- src/test/java/blackjack/CardTest.java
+++ src/test/java/blackjack/CardTest.java
@@ -1,12 +1,30 @@
 package blackjack;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CardTest {
+
+    @Test
+    void 카드는_카드_값으로부터_점수를_계산할_수_있다() {
+        Card card1 = new Card("5");
+        Card card2 = new Card("A");
+        Card card3 = new Card("K");
+
+        int score1 = card1.calculateScore();
+        int score2 = card2.calculateScore();
+        int score3 = card3.calculateScore();
+
+        assertEquals(5, score1);
+        assertEquals(1, score2);
+        assertEquals(10, score3);
+    }
+
 
     @ParameterizedTest
     @ValueSource(strings = {"2", "5", "10"})
